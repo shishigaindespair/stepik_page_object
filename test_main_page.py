e21bf749c7823d5ae156cd5b3fd58d31d76517c1
Shishiga <ostreo@mail.ru>
2024-05-17 12:45:12 +0300

    Lesson 4.21 step 6

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,19 @@
+from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
