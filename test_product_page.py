ea9c92e8b5ac0734218302051aadf6e069e70167
Shishiga <ostreo@mail.ru>
2024-07-12 10:27:23 +0300

    Lesson 4.3 step 10

--- test_product_page.py
+++ test_product_page.py
@@ -1,3 +1,4 @@
+import pytest
 from pages.base_page import BasePage
 from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
@@ -15,6 +16,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
     basket_page.should_be_empty_basket_message()
 
